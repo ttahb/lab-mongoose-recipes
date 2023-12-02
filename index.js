45423264eb8d235933d77c5e0532c79d58ff3255
ttahb <bhattvijay69@hotmail.com>
2023-12-02 13:09:22 +0530

    all iterations done!

--- index.js
+++ index.js
@@ -17,17 +17,34 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
-    Recipe.create({
-      title:'Upma',
-      level: 'Easy Peasy',
-      ingredients: ['rava', 'water', 'onions', 'water', 'oil'],
+    return Recipe.create({
+      title:'Classic_Upma',
+      level: 'Amateur Chef',
+      ingredients: ['2 tbsp oil', '¾ tsp mustard', '1 tsp cumin', '1 tsp urad dal', '1 tsp chana dal'
+      , '1 dried red chilli', 'few curry leaves', '½ onion, finely chopped', '1 inch ginger, finely chopped', 
+      '2 chilli, finely chopped', '½ tsp salt', '3 cup water', '1 tsp sugar', '1 cup rava / semolina', '1 tsp ghee',
+      '½ lemon juice', '2 tbsp coriander, finely chopped'],
       cuisine: 'some cuisine',
       dishType: 'breakfast',
       duration: 10,
-      creator: 'Chef Anil'
+      creator: 'HEBBARS KITCHEN'
      })
   })
-  // .then(()=> mongoose.connection.close())
+  .then((recipe)=> console.log(`first Recipe title - ${recipe.title}`) )
+  .then(()=> Recipe.insertMany(data))
+  .then((data) =>
+    data.forEach(recipe => console.log(`Recipe title: ${recipe.title}`)))
+  .then(() => 
+    Recipe.findOneAndUpdate({title: 'Rigatoni alla Genovese'}, {duration:100}, {new: true})
+  )
+  .then((updatedRecipe) =>  
+    console.log(`updated Recipe: ${updatedRecipe}`)
+  )
+  .then(() =>
+    Recipe.deleteOne( {title: 'Carrot Cake'})
+  )
+  .then(()=> console.log("data deleted"))
+  .then(()=> mongoose.connection.close())
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
